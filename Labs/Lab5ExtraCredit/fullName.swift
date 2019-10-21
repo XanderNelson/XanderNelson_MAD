7435c635f2279169195994de73132f197fe89613
XanderNelson <47230282+XanderNelson@users.noreply.github.com>
2019-10-20 19:38:37 -0600

    Add files via upload

---
+++ Labs/Lab5ExtraCredit/fullName.swift
@@ -0,0 +1,13 @@
+//
+//  fullName.swift
+//  Lab5ExtraCredit
+//
+//  Created by Xander Nelson on 20/10/2019.
+//  Copyright © 2019 Xander Nelson. All rights reserved.
+//
+
+import Foundation
+class Name{
+    var first:String?
+    var last:String?
+}
